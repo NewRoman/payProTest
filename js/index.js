4553766f67079b91337668f470eb85b25b2fac12
Roman <roman.gorbunov@movittech.com>
2020-05-16 22:29:29 +0300

    Styled form block and added greeting msg

--- js/index.js
+++ js/index.js
@@ -1,3 +1,15 @@
-// $.ready(function(){
-//     console.log('here we are')
-// });
+$(document).ready(function() {
+    $('.select2-source').select2();
+    $('.select2-source__no-search').select2({
+        placeholder: "Select a state",
+        minimumResultsForSearch: Infinity
+    });
+    $('.btn-submit-checkout').click(function (event) {
+
+        setTimeout(function () {
+            $('.main-content').hide('slow');
+            $('.greeting').show('slow');
+        }, 300)
+
+    })
+});
